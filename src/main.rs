3608e28f3d21afe83946ed0b5751c7621c00b28b
Jamie Dabu <agonistagent@gmail.com>
2020-12-07 16:06:04 -0600

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,33 @@ use std::collections::HashSet;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead,BufReader};
+use std::iter::FromIterator;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let raw = File::open("./input.txt")?;
     let buf = BufReader::new(raw);
-    let mut s = HashSet::new();
+    let mut s: HashSet<char> = HashSet::new();
     let mut groups = Vec::new();
     let mut sum = 0;
+    let mut start = true;
     for line in buf.lines() {
         let line = line.unwrap();
         if line == "" {
             sum += s.len();
             groups.push(s);
             s = HashSet::new();
+            start = true;
         } else {
-            line.chars().for_each(|c| {s.insert(c); return});
+            if start {
+                start = false;
+                line.chars().for_each(|c| {s.insert(c); return});
+            } else {
+                let person: HashSet<char> = HashSet::from_iter(line.chars());
+                s.retain(|a| person.contains(a));
+            }
         }
     }
-    if(!s.is_empty()){
+    if !s.is_empty() {
         sum += s.len();
         groups.push(s);
     }
